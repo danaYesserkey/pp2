7dcea481672de2d05138346737d0eeab90c89793
dana <d_yesserkey@kbtu.kz>
2025-09-18 14:35:10 +0500

    home

--- week2/w3schools/home.py
+++ week2/w3schools/home.py
@@ -1,1 +1,49 @@
+#1
 print("Hello, World!")
+
+#Python Indentation
+if 5 > 2:
+  print("Five is greater than two!")
+#examples
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#python Variables
+x = 5
+y = "Hello, World!"
+
+""" loooong coment """"
+
+
+#variables
+#EX
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#EX
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#EX 
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#EX type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#same
+x = "John"
+x = 'John'
+#case-sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
